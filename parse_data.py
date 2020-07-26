ab3308e51992cbcbb7e32cd0ffbf58dd2619cea5
Gopinath M <mgopi1990@gmail.com>
2020-07-26 23:46:17 +0530

    Removed unwanted code

--- parse_data.py
+++ parse_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
 import pprint
@@ -9,30 +9,9 @@ Dict = {}
 dateList = []
 for file in os.listdir('xls-data'):
  fp = open('xls-data/' + file, 'r')
- soup = BeautifulSoup(fp, 'html.parser')
- for k in soup.findAll('tr'):
-  row = []
-  for l in k.findAll('td'):
-   val = l.find(text=True)
-   row.append(val)
-  if len(row) < 9:
-   continue
-  date = str(row[0]).replace(' ','')
-  #print (date)
-  if date not in dateList:
-   dateList.append(date)
-  Dict.setdefault(date, {})
-  Dict[date][row[2].rstrip(' ')] = row[9]
- fp.close()
 
 #pprint.pprint(Dict)
 #print(dateList[::-1])
-
-with open('2018.csv', 'w', encoding='utf-8', newline='') as csv_file:
- csv_writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
- for k in dateList[::-1]:
-  for l in Dict[k].keys():
-   csv_writer.writerow([k, l, Dict[k][l]])
 
 
 
